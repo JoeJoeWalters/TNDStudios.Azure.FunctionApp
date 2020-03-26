7ed884786de5a7a2daaa8e3313f074c966bc368a
TNDStudios <info@thenakeddeveloper.com>
2020-03-26 13:08:53 +0000

    Added missing files that were dropped in the move

---
+++ Packages/Security/Results/SecurityResult.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace TNDStudios.Azure.FunctionApp.Security
+{
+
+    public class SecurityResult<T>
+    {
+        public Boolean Initialised { get; set; } = false;
+
+        /// <summary>
+        /// LIst of permissions derived from the token
+        /// </summary>
+        public List<T> Permissions { get; set; } = new List<T>();
+
+        /// <summary>
+        /// Does the current context have permissions of a given type
+        /// </summary>
+        /// <param name="value">The enum item of the pre-defined type</param>
+        /// <returns>If the context has the permission</returns>
+        public Boolean HasPermission(T value) => Initialised ? Permissions.Contains(value) : false;
+    }
+}
---
+++ Packages/Security/Results/TokenResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace TNDStudios.Azure.FunctionApp.Security
+{
+    public class TokenResult
+    {
+        public Boolean Success { get; set; } = false;
+        public ClaimsPrincipal Principal { get; set; } = null;
+    }
+}
